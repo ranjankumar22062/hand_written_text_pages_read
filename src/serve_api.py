25b5406c3133a024bd727d3e1cfbbcb54341e0f0
Ranjan Kumar <meb22062@gamil.com>
2025-08-28 15:10:36 +0530

    changes new

--- src/serve_api.py
+++ src/serve_api.py
@@ -10,7 +10,8 @@ import os
 from src.preprocess import pdf_to_images, preprocess_image
 from src.layout import detect_blocks
 from src.segmentation import segment_lines
-from src.htr_model import load_model, recognize_line
+from src.htr_model import HTRModel
+
 from src.postprocess import clean_text
 from src.pdf_builder import build_pdf
 
